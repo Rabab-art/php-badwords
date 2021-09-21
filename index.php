ea4b713c57dd3278fb0a34b131d4918b0d1ca3ca
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-21 18:34:50 +0200

    Inserimento del parametro GET

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
-$paragraf= "La vita è il dono più grande e prezioso che l'uomo abbia potuto ricevere, è il valore assoluto. La viviamo ogni giorno dato che fa parte di ognuno di noi.Tuttavia non bisogna temere la morte, perché fa parte della nostra vita, non ha senso vivere con la paura di tutto ciò che fa parte della sofferenza.";
+$paragraf= "L'amore è una forma di pregiudizio. Si ama quello di cui si ha bisogno, quello che ci fa star bene, quello che ci fa comodo. Come fai a dire che ami una persona, quando al mondo ci sono migliaia di persone che potresti amare di più, se solo le incontrassi? Il fatto è che non le incontri.";
 
+?>
+
+<?php
+echo 'Citazione di ' . htmlspecialchars($_GET["name"]) . '!';
 ?>
 
 <p><?php echo $paragraf ?> </p>
