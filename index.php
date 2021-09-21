a3b0033d83e86fa642c59769a095db232e624e50
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-21 17:58:18 +0200

    Creazione paragrafo

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php 
+$paragraf= "La vita è il dono più grande e prezioso che l'uomo abbia potuto ricevere, è il valore assoluto. La viviamo ogni giorno dato che fa parte di ognuno di noi.Tuttavia non bisogna temere la morte, perché fa parte della nostra vita, non ha senso vivere con la paura di tutto ciò che fa parte della sofferenza.";
+
 ?>
+
+<p><?php echo $paragraf ?> </p>
